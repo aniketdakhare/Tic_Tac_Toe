d9379fc2bfb47dc7f443596a82519866530b75f0
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-11 13:23:58 +0530

    Selected the Symbol

--- tic_tac_toe.sh
+++ tic_tac_toe.sh
@@ -5,6 +5,8 @@ echo -e "\nWelcome to Tic Tac Toe Game\n"
 #constants
 ROWS=3
 COLUMNS=3
+PLAYER_SYMBOL=''
+COMPUTER_SYMBOL=''
 EMPTY='.'
 
 #variables
@@ -24,16 +26,36 @@ function board_Reset()
    done
 }
 
+function symbol_Selection()
+{
+
+   select_Symbol=$1
+	case $select_Symbol in
+   	1)
+			PLAYER_SYMBOL=X
+			COMPUTER_SYMBOL=O ;;
+      2)
+			PLAYER_SYMBOL=O
+         COMPUTER_SYMBOL=X ;;
+   esac
+	echo -e "\nYour Symbol is '$PLAYER_SYMBOL' and Computer's Symbol is '$COMPUTER_SYMBOL'"
+}
+
+
 function who_is_First()
 {
 	toss=$((RANDOM%2))
 	case $toss in
 		1)
 			chance=$toss
-      	echo "You won the toss. It's your Chance" ;;
+      	echo -e "You won the toss. It's your Chance \nSelect the valid option for symbol: \n\n1. 'X' \n\n2. 'O' "
+			read select_Symbol
+			symbol_Selection $select_Symbol ;;
 		0)
 			chance=$toss
-         echo "Computer Won the toss. It's Computer's Chance" ;;
+         echo -e "Computer Won the toss. It's Computer's Chance\n"
+			select_Symbol=$(($((RANDOM%2))+1))
+			symbol_Selection $select_Symbol ;;
 	esac
 }
 
